a851d4d322ec4c6de3372a0ba4ea9e5e94920a98
Prathamesh patil <prath7972@gmail.com>
2025-08-12 17:34:36 +0530

    12 aug

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+# Create your models here.\
+choices=[
+    ("restaurant","restaurant"),
+    ("customer","customer")
+]
+class Customuser(AbstractUser):
+    role=models.CharField(max_length=255,choices=choices)
+    
--- food_delivery/settings.py
+++ food_delivery/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-]
+    "accounts",
+    "customer",
+    "restaurant"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
