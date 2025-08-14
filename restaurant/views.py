b25068e5ed4c0a5533f5b481cc739cbecef58735
Prathamesh patil <prath7972@gmail.com>
2025-08-14 13:34:36 +0530

    till

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from .models import Dish,RestaurantProfile
 
 # Create your views here.
 def restaurant_dashboard(request):
     return render(request,'restaurant_dashboard.html')
+
+def add_dish(request):
+    data=request.POST
+    is_available=is_available in request.POST
+    restaurant_profile=RestaurantProfile.objects.get(user=request.user)
+    Dish.objects.create(name=data['name'],category=data['category'],price=data['price'],qty=data['qty'],description=data['description'],is_available=is_available,restaurant=restaurant_profile)
+    all_data=Dish.objects.all()
+    return render(request,)
+
+def list_dish(request):
+    data=request.POST
+    all_data=Dish.objects.all()
+    return render(request,)
+    
+
+
+
+
+
+
+
+
+
+    
+
